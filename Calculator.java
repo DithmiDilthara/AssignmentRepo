e78c6143203424ea9b3deb909446030389a27ce3
Dithmi0711 <dithmidilthara0711@gmail.com>
2025-03-06 15:50:09 +0530

    added some changes

--- Calculator.java
+++ Calculator.java
@@ -5,8 +5,8 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-        int num1 = 20;
-        int num2 = 5;
+        int num1 = 60;
+        int num2 = 11;
         
         // Calling the method
         int result = addNumbers(num1, num2);
