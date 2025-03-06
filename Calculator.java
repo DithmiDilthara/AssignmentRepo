d59526f6f843d3fc0d0cb4bf29bdbb0e218e9ffb
Dithmi0711 <dithmidilthara0711@gmail.com>
2025-03-06 15:39:43 +0530

    Added files

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    // Method to add two numbers
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        
+        // Calling the method
+        int result = addNumbers(num1, num2);
+
+        // Displaying the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
